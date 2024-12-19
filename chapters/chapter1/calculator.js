6b892743a223b9f45887f422ed6b71a74f3d86c6
Michael Gauthier <mikegauthier222@hotmail.com>
2024-12-18 20:55:32 -0800

    start calculator/finish datatypes

---
+++ chapters/chapter1/calculator.js
@@ -0,0 +1,16 @@
+// A basic calculator
+
+let pro = process.argv
+
+for (let i = 2; i < pro.length; i++) {
+  const element = pro[i];
+  console.log(element);
+}
+
+const calculator = (input) => {
+  for (let i = 0; i < pro.length; i++) {
+    const element = pro[i];
+    console.log(element);
+  }
+}
+
--- chapters/chapter1/datatypes.js
+++ chapters/chapter1/datatypes.js
@@ -1,6 +1,23 @@
 // A funtion that checks data types
 
-const checkType = function(input) {
+const checkType = function (input) {
   // typeof doesn't check for null values specifically and will return object
+  if (input === null) {
+    return 'null';
+  // special check for an array
+  } else if (Array.isArray(input) === true) {
+    return 'array'
+  }
+  return typeof input;
+};
 
-}
+console.log(checkType("hello"));
+console.log(checkType(1));
+console.log(checkType(null));
+console.log(checkType());
+console.log(checkType(9n));
+console.log(checkType(Symbol("foo")));
+console.log(checkType(true));
+console.log(checkType({ foo: 1 , bar: 2}));
+console.log(checkType([1, 2, 3]));
+
