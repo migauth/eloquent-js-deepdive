8bbede49387d9694f2685cf69987d9e9831dc76c
Michael Gauthier <mikegauthier222@hotmail.com>
2024-12-20 09:08:22 -0800

    add check for zero or single numbers

--- chapters/chapter1/calculator.js
+++ chapters/chapter1/calculator.js
@@ -1,17 +1,14 @@
 // A command line calculator
 
-// const prompt = require('prompt-sync')();
-
-// let choice = prompt("Would you like to: add, subtract, multiply, divide? ");
-
-let userInput = process.argv
+const prompt = require('prompt-sync')();
 
 const looper = (input) => {
   let arr = [];
-  for (let i = 2; i < input.length; i++) {
+  for (let i = 0; i < input.length; i++) {
     const element = input[i];
     arr.push(Number(element))
   }
+  // console.log('looper',arr);
   return arr;
 }
 
@@ -19,6 +16,30 @@ const add = (input) => {
   let sum = 0;
   return input.reduce((acc, val) => acc + val, sum)
 }
+
+const sub = (input) => {
+  let dif = 0;
+  return input.reduce((acc, val) => acc - val, dif)
+}
+
+const choice = prompt("Would you like to: add, subtract, multiply, divide? ");
+
+if (choice === 'add') {
+  let nums = prompt("What numbers would you like to add? ")
+  if (nums.length <= 1){
+    console.log("need more numbers!");
+  } else {
+  let decode = nums.split(' ')
+  console.log(add(looper(decode)))
+  }
+}
+
+if (choice === 'subtract') {
+  let nums = prompt("What numbers would you like to subtract? ")
+  let decode = nums.split(' ')
+  console.log(sub(looper(decode)))
+}
+
 
 // const calculator = (input) => {
 //   let result = 0;
@@ -28,4 +49,4 @@ const add = (input) => {
 //   }
 // }
 
-console.log(add(looper(userInput)));
+// console.log(add(looper(userInput)));
