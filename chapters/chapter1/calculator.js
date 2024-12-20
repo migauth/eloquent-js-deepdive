beec563b51771bbaf9d1cfd8262b5d3d16038c28
Michael Gauthier <mikegauthier222@hotmail.com>
2024-12-20 10:13:09 -0800

    add subtract and default to switch

--- chapters/chapter1/calculator.js
+++ chapters/chapter1/calculator.js
@@ -1,52 +1,50 @@
 // A command line calculator
 
-const prompt = require('prompt-sync')();
+const prompt = require("prompt-sync")();
 
 const looper = (input) => {
   let arr = [];
   for (let i = 0; i < input.length; i++) {
     const element = input[i];
-    arr.push(Number(element))
+    arr.push(Number(element));
   }
   // console.log('looper',arr);
   return arr;
-}
+};
 
 const add = (input) => {
   let sum = 0;
-  return input.reduce((acc, val) => acc + val, sum)
-}
+  return input.reduce((acc, val) => acc + val, sum);
+};
 
 const sub = (input) => {
   let dif = 0;
-  return input.reduce((acc, val) => acc - val, dif)
-}
+  return input.reduce((acc, val) => acc - val, dif);
+};
 
 const choice = prompt("Would you like to: add, subtract, multiply, divide? ");
 
-if (choice === 'add') {
-  let nums = prompt("What numbers would you like to add? ")
-  if (nums.length <= 1){
-    console.log("need more numbers!");
-  } else {
-  let decode = nums.split(' ')
-  console.log(add(looper(decode)))
-  }
+console.log(`What numbers would you like to ${choice}?`);
+
+switch (choice) {
+  case "add":{
+    let nums = prompt("");
+    if (nums.length <= 1) {
+      console.log("need more numbers!");
+    } else {
+      let decode = nums.split(" ");
+      console.log(add(looper(decode)));
+    }}
+    break;
+  case "subtract":{
+    let nums = prompt("");
+    if (nums.length <= 1) {
+      console.log("need more numbers!");
+    } else {
+      let decode = nums.split(" ");
+      console.log(sub(looper(decode)));
+    }}
+    break;
+  default:
+    console.log("Command does not exist");
 }
-
-if (choice === 'subtract') {
-  let nums = prompt("What numbers would you like to subtract? ")
-  let decode = nums.split(' ')
-  console.log(sub(looper(decode)))
-}
-
-
-// const calculator = (input) => {
-//   let result = 0;
-//   for (let i = 2; i < input.length; i++) {
-//     const element = input[i];
-//     console.log(typeof element);
-//   }
-// }
-
-// console.log(add(looper(userInput)));
