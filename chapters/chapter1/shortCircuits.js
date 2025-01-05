d34e71a5380b250f67b21a480278204cb98cf151
Michael Gauthier <mikegauthier222@hotmail.com>
2025-01-05 07:36:00 -0800

    add proper && short circuit

--- chapters/chapter1/shortCircuits.js
+++ chapters/chapter1/shortCircuits.js
@@ -12,17 +12,20 @@ function test(input) {
 }
 console.log(test('test'));
 console.log(test());
+console.log(test(""));
+console.log(test(1));
+console.log(test(NaN));
 
 // test 2 won't work the same because both are true, so it returns 'default' because both values are truthy; it returns the value on the right
 // in the second log because input is undefined it is false so it 'short circuits' and runs nothing
 console.log('\n---test2---');
 function test2(input) {
-  return input && 'default';
+  return input && 'default'; //reversing this does not work
 }
 console.log(test2('test'));
 console.log(test2());
 
-// test 3 returns 'test' because both values are true, && returns what is on the right
+// test 3 returns 'test' when both values are true, && returns what is on the right
 console.log('\n---test3---');
 function test3(isInput, input) {
   return isInput && input;
@@ -30,7 +33,7 @@ function test3(isInput, input) {
 console.log(test3(true, 'test'));
 console.log(test3(false, 'test'));
 console.log(test3(0,'test')); //returns the falsey value 0 but not false
-console.log(test3('what','test'));
+console.log(test3('foo','test'));
 
 
 
