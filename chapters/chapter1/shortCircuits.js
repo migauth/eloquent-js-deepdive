efa89644b579aacb7b045fc67e2a0f1ff00b50db
Michael Gauthier <mikegauthier222@hotmail.com>
2025-01-04 16:30:40 -0800

    more short circuits

--- chapters/chapter1/shortCircuits.js
+++ chapters/chapter1/shortCircuits.js
@@ -1,6 +1,38 @@
+/* 
+&& logical operator both must be true
+(true && true)
+*/
+
+
+// if input is true or if there is a value, it will be returned
+// if there is no input, 'default' will be true so it will run
+console.log('---test1---');
 function test(input) {
   return input || 'default';
 }
-
 console.log(test('test'));
-console.log(test(''));
+console.log(test());
+
+// test 2 won't work the same because both are true, so it returns 'default' because both values are truthy; it returns the value on the right
+// in the second log because input is undefined it is false so it 'short circuits' and runs nothing
+console.log('\n---test2---');
+function test2(input) {
+  return input && 'default';
+}
+console.log(test2('test'));
+console.log(test2());
+
+// test 3 returns 'test' because both values are true, && returns what is on the right
+console.log('\n---test3---');
+function test3(isInput, input) {
+  return isInput && input;
+}
+console.log(test3(true, 'test'));
+console.log(test3(false, 'test'));
+console.log(test3(0,'test')); //returns the falsey value 0 but not false
+console.log(test3('what','test'));
+
+
+
+
+
