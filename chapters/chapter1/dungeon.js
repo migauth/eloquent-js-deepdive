2a4e42e4ae007fe7debeb49cf437ee6e6fb42660
Michael Gauthier <mikegauthier222@hotmail.com>
2025-01-06 08:15:05 -0800

    rename

--- chapters/chapter1/dungeon.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")();
-
-const start = prompt("You wake up in a dark room. What do you do? ").trim().toLowerCase();
-
-function noInput(input) {
-  return input || 'you lose'
-}
-
-console.log(noInput(start));
