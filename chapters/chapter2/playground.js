572981eec7820dec7d675434239fb76cef250506
Michael Gauthier <mikegauthier222@hotmail.com>
2025-01-13 12:05:57 -0800

    add playground

---
+++ chapters/chapter2/playground.js
@@ -0,0 +1,25 @@
+const prompt = require("prompt-sync")();
+
+let username;
+
+// let count = 0
+// let result = 1
+
+while (true) {
+console.log('hello');
+}
+
+// This gets stuck in a loop that can't be terminated
+
+// while (!username) {
+//   prompt('Enter your usename: ')
+//   console.log(username);
+// }
+
+/** 
+ * Used: ps aux | grep node
+ * to look thru terminal process
+ * or: ps aux | grep -i terminal
+ * in case there is something running
+ * or just close the terminal...
+ * */ 
