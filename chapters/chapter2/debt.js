8424ee502d9607fdcbe58c0ef9a43de65d277b5e
Michael Gauthier <mikegauthier222@hotmail.com>
2025-01-20 09:23:41 -0800

    use recursion to loop question until answered

--- chapters/chapter2/debt.js
+++ chapters/chapter2/debt.js
@@ -2,16 +2,64 @@ const readline = require("readline");
 
 let rl = readline.createInterface(process.stdin, process.stdout);
 
-let mike = 0;
-let ember = 0;
+let mikeTotal = 0;
+let emberTotal = 0;
 
-rl.question("Who paid? ", (who) => {
-  rl.question("How much? ", (pay) => {
-    mike = pay
-    console.log(`Ember owes Mike ${mike}`);
+// Add do while loops to repeat question until its answered?
+// Gets stuck in an infinite loop. Must use recursive funtion.
+
+// let whoPaid;
+
+// do {
+// whoPaid = rl.question("Who paid", (who)=> {
+//   })
+// } while (!whoPaid) {
+//   console.log(`${who} paid`);
+// }
+
+function askWhoPaid() {
+  // let who;
+  rl.question("Who paid? ", (who) => {
+    const low = who.toLocaleLowerCase();
+    if (low !== "mike" && low !== "ember") {
+      console.log("name not in system");
+      askWhoPaid();
+    }
+    console.log(who);
   });
-});
+}
 
+askWhoPaid()
 
+// rl.question("Who paid? ", (who) => {
+// const low = who.toLocaleLowerCase()
+//   // if the lowercase of the string does not equal 'mike' and it does not equal 'ember' then the condition will be true and it will run
+// if (low !== 'mike' && low !== 'ember') {
+//   console.log('name not in system');
+//   rl.close()
+//   return;
+//   }
 
+//   rl.question("How much? ", (pay) => {
 
+//     rl.question("Full or split?", (ans) => {
+//       const lowAns = ans.toLowerCase();
+//       if (lowAns !== 'full' && lowAns !== 'split') {
+//         console.log('Option not valid');
+//       }
+
+//     })
+
+//     if (low === 'mike') {
+//       mikeTotal = pay
+//       console.log(`Ember owes ${who} $${mikeTotal / 2}`);
+//     }
+
+//     if (low === 'ember') {
+//       emberTotal = pay
+//       console.log(`Mike owes ${who} $${emberTotal / 2}`);
+//     }
+
+//     rl.close()
+//   });
+// });
