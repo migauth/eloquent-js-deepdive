97ebea8f4158da85fa1fc02cbf6460e1dd3ac212
Michael Gauthier <mikegauthier222@hotmail.com>
2025-11-16 18:04:29 -0500

    return object from function

--- chapters/chapter4/objects.js
+++ chapters/chapter4/objects.js
@@ -2,7 +2,25 @@ const testObject = {
   name : 'Mike',
   age : 36,
   isCool : true,
-  sayHi : () => {console.log(`hi, I'm Mike`);} //method
+  isWearing : ['pants', 'shirt', 'underwear'],
+  sayHi : (say) => {console.log(`${say}`);} //method3
 }
 
-testObject.sayHi();
+testObject.sayHi('hi');
+
+testObject.pizza = 'peperoni'
+testObject.name = "Michael"
+
+
+// console.log(Object.keys(testObject));
+// console.log(Object.values(testObject));
+
+const sayMyName = function sayMyName(name) {
+  return {
+    isCool : true,
+    myName : name.toUpperCase(),
+  }
+}
+
+console.log(sayMyName("mike"));
+console.log(sayMyName("mike").myName);
